2db28fe210698e515c2226cca24f4190eef98d67
杨艺辉 <122757@nd.com>
2017-03-06 19:32:02 +0800

    no message

--- conn/getCategory.php
+++ conn/getCategory.php
@@ -6,7 +6,7 @@
 	 * 表的关联 限制条数  不同分类的查询 
 	 *  文章id 文章作者 时间 文章分类 文章标题 文章简介(摘要)
 	 */
-	$sql="select wp_posts.ID,post_date,post_title,post_excerpt,wp_users.user_nicename from wp_posts inner join wp_users where post_status='publish' and wp_users.ID=wp_posts.post_author";
+	$sql="select wp_posts.ID,post_date,post_title,post_excerpt,wp_usermeta.meta_value from wp_posts inner join wp_users where post_status='publish' and wp_users.ID=wp_posts.post_author";
 	$result=mysql_query($sql,$conn);
 	if(!!$result){
 		while($rs = mysql_fetch_assoc($result)){
